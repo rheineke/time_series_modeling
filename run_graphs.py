576f6dc2b4717844564f2911943c94718fa2c872
Reece Heineke <reece.heineke@gmail.com>
2017-02-20 13:42:22 -0500

    ENH: Exploratory data analysis in python notebook (#1)

--- run_graphs.py
+++ run_graphs.py
@@ -6,7 +6,7 @@ from sklearn.externals import joblib
 
 if __name__ == '__main__':
     clf_name = 'decisiontreeregressor'
-    model_date = dt.date.today()
+    model_date = dt.date(2017, 2, 19)
     pipe_name = 'Pipe_{}'.format(clf_name)
 
     # Read pickled/joblib model
@@ -17,14 +17,15 @@ if __name__ == '__main__':
     # Create graphviz dot data, transform to pdf and write to file
     export_kwargs = dict(
         out_file=None,
+        # rotate=True,
         filled=True,
         rounded=True,
         special_characters=True
     )
     dot_data = tree.export_graphviz(clf, **export_kwargs)
     graph = pydotplus.graph_from_dot_data(dot_data)
-    pdf_fmt = 'output/{}-{:%y-%m-%d}.pdf'
-    graph.write_pdf(pdf_fmt.format(pipe_name, model_date))
+    png_fmt = 'output/{}-{:%y-%m-%d}.png'
+    graph.write_png(png_fmt.format(pipe_name, model_date))
 
     # From the command line, run:
     # dot -Tpdf output/Pipe_decisiontreeregressor.dot -o output/Pipe_decisiontreeregressor.pdf
